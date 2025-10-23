45af2e6f8d5c801d9053a52b8c076753e290b700
Muhammad Arkan Ramadhan <2511500056@mahasiswa.atmaluhur.ac.id>
2025-10-23 12:38:15 +0700

    Hitung Jumlah Karakter Pesan

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -8,9 +8,6 @@ document.getElementById("menuToggle").addEventListener("click", function () {
   }
 });
 
-document.getElementById("menuToggle").addEventListener("click", function () {
-  document.querySelector("nav").classList.toggle("active");
-});
 document.querySelector("form").addEventListener("submit", function (e) {
   const nama = document.getElementById("txtNama");
   const email = document.getElementById("txtEmail");

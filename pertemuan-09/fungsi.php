b478c9b0300b67d9323b714115235c8e683d8e83
Muhammad Arkan Ramadhan <2511500056@mahasiswa.atmaluhur.ac.id>
2025-11-27 12:28:24 +0700

    Modif proses untuk array kontak kami

--- pertemuan-09/fungsi.php
+++ pertemuan-09/fungsi.php
@@ -13,13 +13,13 @@ return date("d M Y", strtotime($tgl));
 }
 function tampilkanBiodata($conf, $arr)
 {
- $html = "";
- foreach ($conf as $k => $v) {
-  $label = $v["label"];
-  $nilai = bersihkan($arr[$k] ?? '');
-  $suffix = $v["suffix"];
+$html = "";
+foreach ($conf as $k => $v) {
+ $label = $v["label"];
+ $nilai = bersihkan($arr[$k] ?? '');  $suffix = $v["suffix"];
 
-  $html .= "<p><strong>{$label}</strong> {$nilai}{$suffix}</p>";
- }
- return $html;
+ $html .= "<p><strong>{$label}</strong> {$nilai}{$suffix}</p>";
 }
+return $html;
+}
+?>

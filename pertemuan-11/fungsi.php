e632d87630bf220487b2ea29094d121e3d3cd8f2
Muhammad Arkan Ramadhan <2511500056@mahasiswa.atmaluhur.ac.id>
2025-12-11 11:21:19 +0700

    INSERT tbl_tamu dengan prepared statement mysqli dan simpan pesan sukses/gagal ke  serta redirect balik ke index.php#contact

--- pertemuan-11/fungsi.php
+++ pertemuan-11/fungsi.php
@@ -20,7 +20,7 @@ function tampilkanBiodata($conf, $arr)
   $html = "";
   foreach ($conf as $k => $v) {
     $label = $v["label"];
-    $nilai = bersihkan($arr[$k] ?? '');
+    $nilai = bersihkan($arr[$k] '');
     $suffix = $v["suffix"];
 
     $html .= "<p><strong>{$label}</strong> {$nilai}{$suffix}</p>";

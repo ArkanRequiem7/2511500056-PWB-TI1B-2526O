273c9c871f2e11e1057a24511549a3448880c9b5
Muhammad Arkan Ramadhan <2511500056@mahasiswa.atmaluhur.ac.id>
2025-12-18 11:04:12 +0700

    Menambahkan tombol edit di read.php

--- pertemuan-12/read.php
+++ pertemuan-12/read.php
@@ -7,19 +7,23 @@ $no = 1;
 ?>
 <table border="1" cellpadding="8" cellspacing="0">
     <tr>
-        <th>No</th> <th>ID</th>
+        <th>No</th> 
+        <th>Aksi</th> <th>ID</th>
         <th>Nama</th>
         <th>Email</th>
         <th>Pesan</th>
-        <th>Created At</th> </tr>
+        <th>Created At</th> 
+    </tr>
 
 <?php while ($row = mysqli_fetch_assoc($q)): ?>
     <tr>
-        <td><?= $no++; ?></td> <td><?= $row['cid']; ?></td>
-        <td><a href="edit.php?cid=<?= (int)$row['cid']; ?>">Edit"</a></td>
+        <td><?= $no++; ?></td>
+        <td><a href="edit.php?cid=<?= (int)$row['cid']; ?>">Edit</a></td>
+        <td><?= $row['cid']; ?></td>
         <td><?= htmlspecialchars($row['cnama']); ?></td>
         <td><?= htmlspecialchars($row['cemail']); ?></td>
         <td><?= nl2br(htmlspecialchars($row['cpesan'])); ?></td>
-        <td><?= $row['dcreated']; ?></td> </tr>
+        <td><?= isset($row['dcreated']) ? $row['dcreated'] : 'Format Tanggal Salah'; ?></td> 
+    </tr>
 <?php endwhile; ?>
 </table>
